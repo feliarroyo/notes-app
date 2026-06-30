63bb77a9e944bf304519395f0f664a631fdf2966
feliarroyo <farroyo@alumnos.exa.unicen.edu.ar>
2026-06-30 08:22:41 -0300

    feat: Migrating database to PostgreSQL via Docker

--- backend/notes/src/main/java/com/ensolvers/notes/NotesApplication.java
+++ backend/notes/src/main/java/com/ensolvers/notes/NotesApplication.java
@@ -1,5 +1,8 @@
 package com.ensolvers.notes;
 
+
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +10,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class NotesApplication {
 
 	public static void main(String[] args) {
+		// Prevents issues with time zone between Windows and Linux (Docker)
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+		
 		SpringApplication.run(NotesApplication.class, args);
 	}
 
